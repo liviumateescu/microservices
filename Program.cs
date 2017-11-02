96ff584e97ce7f0704411c1b8900751755bb6bda
Liviu Mateescu <liviumateescu@hotmail.com>
2017-11-02 23:16:24 +0200

    First

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder().UseKestrel().UseStartup<Startup>().UseConfiguration(config).Build();
+            host.Run();
+            //Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env) { }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+    }
+}
